18935441e7e3f3c42fec97f30326051b4f14d721
giriraj885 <girirajpatoliya539@gmail.com>
2020-03-15 01:02:49 +0530

    1)Fix the lowercase issue

--- mini_project/miniproject_user/views.py
+++ mini_project/miniproject_user/views.py
@@ -221,7 +221,7 @@ class ManageUser(BaseAPIView):
 
         user_list = User.objects.filter(
             ~Q(phone__in = phone_list)
-        ).order_by('-business_name')     
+        ).extra(select={'lower_name':'lower(business_name)'}).order_by('lower_name')
 
         if 'page_no' in request.GET and request.GET['page_no']!='0':
             page_end = int(request.GET['page_no']) * user_per_page
@@ -321,21 +321,19 @@ class GetAllUser(BaseAPIView):
         page_end = user_per_page
         user_list = []
         
-
         credit_user_list = AccountManagement.objects.filter(
             Q(credit_user=request.user),
             ~Q(debit_user=request.user)
-        ).order_by('-updated_date_time').values_list('debit_user',flat=True).distinct('debit_user')
-
+        ).values_list('debit_user',flat=True).distinct('debit_user')
+        
         debit_user_list = AccountManagement.objects.filter(
             ~Q(credit_user=request.user),
             Q(debit_user=request.user)
-        ).order_by('-updated_date_time').values_list('credit_user',flat=True).distinct('credit_user')
-
+        ).values_list('credit_user',flat=True).distinct('credit_user')
         user_list = User.objects.filter(
             Q(id__in = credit_user_list)|
             Q(id__in = debit_user_list)
-        )
+        ).order_by('-updated_date_time')
 
         if 'page_no' in request.GET and request.GET['page_no']!='0':
             page_end = int(request.GET['page_no']) * user_per_page
